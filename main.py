5bc4223f80479b4abc61c82604b1a91b4b460ddd
王丹彤(Dantong Wang) <wangdantong@genomics.cn>
2024-03-13 16:39:51 +0800

    add comments in main

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+"""this is an example to run on 192.168.66.203"""
 import get_alignments
 pdb_path = '/dellfsqd2/ST_OCEAN/USER/wangdantong/python_toolbox_test/prostac/pdb_files'
 us_path = '/dellfsqd2/ST_OCEAN/USER/wangdantong/toolboxes/usalign/USalign/USalign'
